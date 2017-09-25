3372054edc3ad14fa1d0fe9e8afbd6f8ef999989
Sriganesh Nagaraj <sriganeshharitz@gmail.com>
2017-09-25 23:36:49 +0530

    first commit

---
+++ src/main/java/com/uttarainfo/test/MailIt/MailItApplication.java
@@ -0,0 +1,12 @@
+package com.uttarainfo.test.MailIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailItApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uttarainfo/test/MailIt/MailItApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uttarainfo.test.MailIt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailItApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
